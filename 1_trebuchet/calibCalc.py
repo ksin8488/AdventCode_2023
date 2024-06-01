5d8cf8675f0bba96761eb6eec322da3e48cf0fb3
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2024-05-31 18:45:27 -0600

    Created a testing file and coded the main portion so you can select a file or user can give a line. Solved Advent code with this.

--- 1_trebuchet/calibCalc.py
+++ 1_trebuchet/calibCalc.py
@@ -6,15 +6,38 @@ lines left in the file to be calculated.
 """
 
 import fileinput
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(
+    description="May take a file to read each line for calibration numbers."
+)
+
+parser.add_argument(
+    "-f", "--file", "--filepath", metavar="file",
+    required=False, help="Filepath for a file containing lines with at least two numbers on each line for calibration code calculating."
+)
+
+args = parser.parse_args()
+print("Input file:", args.file)
 
 #--File section--
 
-#opens the "generatedCodes.txt" file to begin processing lines
-#calibration_file = open('generatedCodes', 'r', encoding="utf-8")
-total_num = 0
-
 #TODO: start reading the file line by line and use another function to start adding values to a final total
-
+def get_file():
+    total_num = 0
+    
+    try:
+        file = open(args.file, 'r')
+        for line in file:
+            total_num += calculate_num(line)
+        
+        print("Calibration value is:", total_num)
+        file.close()
+    except FileNotFoundError:
+        print("File not found error. Please check the filepath again")
+        sys.exit(1) #Exit with code 1 to indicate error
+        
 #--Functions--
 def calculate_num(line):
     """
@@ -27,8 +50,29 @@ def calculate_num(line):
         line_value: The two digit number formed from first and last digits found in the line.
     """
     #TODO: Take a string and return a two digit number from the first and last digit found. 
-    line_value = 0 #TODO: Actually code this
-    return line_value
+    first_num = 0
+    last_num = 0
+    num_list = []
+    
+    for char in line:
+        if(char.isdigit()):
+            num_list.append(char)
+    
+    first_num = num_list[0]
+    last_num = num_list[-1] #-1 goes backwards from the starting value so it will be the last element in the list
+    
+    #concatonate first and last digit to form 2 digit num
+    line_value = str(first_num) + str(last_num)
+    return int(line_value)
 
+if args.file is not None:
+    get_file()
+else:
+    user_line = input("Please enter a string with at least two numbers contained anywhere in it.\n")
+    total_num = 0
+    total_num = calculate_num(user_line)
+    
+    print("Calibration value is:", total_num)
+    
 #--Error checking section--
     #TODO: extra section added for practicing error handling. What if there's only one digit? No digits? No lines in file? etc.
